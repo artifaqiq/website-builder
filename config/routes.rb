893acc7cdab15f8a5e0364f54e0ba9b54db448e2
Artur Lomako <artifaqiq@gmail.com>
2016-09-29 17:46:20 +0300

    added authenticate vkontakte

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  devise_for :users, :controllers => { :omniauth_callbacks => "callbacks" }
+  resources :sites
+  root 'sites#index'
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get '/api' => 'application#index', defaults: { format: :json }
 end
